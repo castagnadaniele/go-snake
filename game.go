82d0447b6a6728a81c2d1e864bc62ff1fbcacdba
castagnadaniele <danielecastagna@posteo.net>
2021-07-04 11:30:57 +0200

    add game SendMove to handle direction changes

--- game.go
+++ game.go
@@ -7,13 +7,15 @@ type Game struct {
 	snake        Snake
 	cloak        Cloak
 	coordinatesC chan []Coordinate
+	movesC       chan Direction
 }
 
 // NewGame returns a pointer to Game, which handles snake
 // methods on cloak ticks
 func NewGame(snake Snake, cloak Cloak) *Game {
 	coordinatesChannel := make(chan []Coordinate)
-	return &Game{snake, cloak, coordinatesChannel}
+	movesChannel := make(chan Direction)
+	return &Game{snake, cloak, coordinatesChannel, movesChannel}
 }
 
 // Start starts cloak to tick every d time.Duration,
@@ -22,17 +24,32 @@ func NewGame(snake Snake, cloak Cloak) *Game {
 // the internal channel
 func (g *Game) Start(d time.Duration) {
 	g.cloak.Start(d)
-	go func() {
-		defer close(g.coordinatesC)
-		for range g.cloak.Tick() {
-			g.snake.Move(Left)
+	go g.eventRoutine()
+}
+
+func (g *Game) eventRoutine() {
+	defer close(g.coordinatesC)
+	defer close(g.movesC)
+	direction := g.snake.Face()
+	for {
+		select {
+		case <-g.cloak.Tick():
+			g.snake.Move(direction)
 			g.coordinatesC <- g.snake.GetCoordinates()
+		case d := <-g.movesC:
+			direction = d
 		}
-	}()
+	}
 }
 
 // Coordinates returns a []Coordinate receiver channel
 // which exposes snake moves after every new tick
 func (g *Game) Coordinates() <-chan []Coordinate {
 	return g.coordinatesC
+}
+
+// SendMove sends d Direction to the internal Direction channel
+// which will be pooled inside the Start go routine to change snake direction
+func (g *Game) SendMove(d Direction) {
+	g.movesC <- d
 }
--- game_test.go
+++ game_test.go
@@ -8,14 +8,18 @@ import (
 )
 
 func TestGame(t *testing.T) {
+	width, height := 60, 60
+
 	t.Run("should start game", func(t *testing.T) {
-		width, height := 60, 60
 		s := snake.NewSnake(width, height)
-		cloak := NewStubCloak(2)
+		cloak := NewStubCloak()
+		defer cloak.Stop()
 		g := snake.NewGame(s, cloak)
 		g.Start(time.Microsecond)
 
+		cloak.AddTick()
 		<-g.Coordinates()
+		cloak.AddTick()
 		got := <-g.Coordinates()
 		want := []snake.Coordinate{
 			{34, 30},
@@ -24,28 +28,59 @@ func TestGame(t *testing.T) {
 		}
 		snake.AssertCoordinates(t, got, want)
 	})
+
+	t.Run("snake should change direction", func(t *testing.T) {
+		s := snake.NewSnake(width, height)
+		cloak := NewStubCloak()
+		defer cloak.Stop()
+		g := snake.NewGame(s, cloak)
+		g.Start(time.Microsecond)
+
+		cloak.AddTick()
+		got := <-g.Coordinates()
+		want := []snake.Coordinate{
+			{35, 30},
+			{36, 30},
+			{37, 30},
+		}
+		snake.AssertCoordinates(t, got, want)
+
+		g.SendMove(snake.Up)
+		cloak.AddTick()
+		got = <-g.Coordinates()
+		want = []snake.Coordinate{
+			{35, 29},
+			{35, 30},
+			{36, 30},
+		}
+		snake.AssertCoordinates(t, got, want)
+	})
 }
 
 type StubCloak struct {
-	Ticks int
-	C     chan time.Time
+	C        chan time.Time
+	now      time.Time
+	i        int
+	duration time.Duration
 }
 
-func NewStubCloak(ticks int) *StubCloak {
-	ticker := make(chan time.Time, ticks)
-	return &StubCloak{ticks, ticker}
+func NewStubCloak() *StubCloak {
+	ticker := make(chan time.Time)
+	return &StubCloak{ticker, time.Now(), 0, time.Nanosecond}
 }
 
 func (c *StubCloak) Start(d time.Duration) {
-	defer close(c.C)
-	now := time.Now()
-	for i := 0; i < c.Ticks; i++ {
-		c.C <- now.Add(time.Duration(i) * d)
-	}
+	c.duration = d
+}
+
+func (c *StubCloak) AddTick() {
+	c.C <- c.now.Add(time.Duration(c.i) * c.duration)
 }
 
 func (c *StubCloak) Tick() <-chan time.Time {
 	return c.C
 }
 
-func (c *StubCloak) Stop() {}
+func (c *StubCloak) Stop() {
+	close(c.C)
+}
